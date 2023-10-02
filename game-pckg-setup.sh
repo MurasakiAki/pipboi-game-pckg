e6429da5d4b536ef38a4771cc1a373ab1a1621f6
MurasakiAki <123252526+MurasakiAki@users.noreply.github.com>
2023-10-02 18:36:42 +0200

    idkkk help me pls

---
+++ game-pckg-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pipboi_path=$(find / -type d -name "pipboi" 2>/dev/null)
+
+if [ -n "$pipboi_path" ]; then
+    echo pipboi exists
+    mv "$PWD" "$pipboi_path"
+    echo Succesfully integrated pipboi-game-pckg into PIPBOI system
+fi
