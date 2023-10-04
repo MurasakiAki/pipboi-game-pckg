cd9481c0d4a8a5930d5624ff53a9d5fd4fc3ac50
MurasakiAki <akkishia88@gmail.com>
2023-10-04 11:06:14 +0200

    basic login

--- wastelanders/main.py
+++ wastelanders/main.py
@@ -15,13 +15,30 @@ def check_integration():
         return False
 
 def check_player(username):
-    return os.path.exists(f"{os.getcwd()}/wastelanders/.{username}")
+    return os.path.exists(f"{os.getcwd()}/wastelanders/.{username}.ini")
 
 def register(username, password):
-    pass
+    if check_player(username):
+        print("Player already exists")
+    else:
+        try:
+            with open(f"{os.getcwd()}/wastelanders/.{username}.ini", 'x') as file:
+                file.write(f"[Data]\npassword={password}")
+        except:
+            pass
+        print("New player created.")
 
 def login(username, password):
-    pass
+    if check_player(username):
 
+        player_config = configparser.ConfigParser()
+        player_config.read('.{username}.ini')
+
+        read_pass = player_config.getint('Data', 'password')
+
+        if read_pass == password:
+            print("login successful")
+        else:
+            print("invalid password")
 print(check_player("aki"))
 time.sleep(10)
