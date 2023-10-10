7cd9dfe0097aa8a6e8e419092cdf61a79766f497
MurasakiAki <123252526+MurasakiAki@users.noreply.github.com>
2023-10-10 20:52:00 +0200

    commit my balls

--- wastelanders/game.py
+++ wastelanders/game.py
@@ -1,10 +1,13 @@
 from main import read_ini, write_ini
 
 def create_character(usrnm, rc, clss):
-    # Stats
+    # Base Stats
     write_ini(f'wastelanders/.{usrnm}.ini','Stats', 'race', rc)
     write_ini(f'wastelanders/.{usrnm}.ini','Stats', 'class', clss)
     write_ini(f'wastelanders/.{usrnm}.ini','Stats', 'exp', 0)
     write_ini(f'wastelanders/.{usrnm}.ini','Stats', 'level', 1)
- 
+
+    # Clss/RC based stats
+    if clss
+
 create_character('aki', 'loli', 'sniper')
