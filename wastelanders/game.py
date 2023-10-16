35c4b9043b03177d551671a9d35935cd54041791
MurasakiAki <123252526+MurasakiAki@users.noreply.github.com>
2023-10-16 17:24:43 +0200

    cool ideaz

--- wastelanders/game.py
+++ wastelanders/game.py
@@ -1,5 +1,17 @@
 from main import read_ini, write_ini
 
+def fill_rc_stats(usrnm, hp, stmn, str, dex, per, agi, spt_dst):
+    write_ini(f'wastelanders/.{usrnm}.ini','Stats', 'health', hp)
+    write_ini(f'wastelanders/.{usrnm}.ini','Stats', 'stamina', stmn)
+    write_ini(f'wastelanders/.{usrnm}.ini','Stats', 'STR', str)
+    write_ini(f'wastelanders/.{usrnm}.ini','Stats', 'DEX', dex)
+    write_ini(f'wastelanders/.{usrnm}.ini','Stats', 'PER', per)
+    write_ini(f'wastelanders/.{usrnm}.ini','Stats', 'AGI', agi)
+    write_ini(f'wastelanders/.{usrnm}.ini','Stats', 'spot_dist', spt_dst)
+
+def fill_clss_stats(usrn, mss, bckpck, hd, chst, lgs, lft_hnd, rght_hnd):
+    
+
 def create_character(usrnm, rc, clss):
     # Base Stats
     write_ini(f'wastelanders/.{usrnm}.ini','Stats', 'race', rc)
@@ -8,6 +20,18 @@ def create_character(usrnm, rc, clss):
     write_ini(f'wastelanders/.{usrnm}.ini','Stats', 'level', 1)
 
     # Clss/RC based stats
-    if clss
+    if rc == "human":
+        fill_rc_stats(usrnm, 100, 100, 5, 5, 5, 5, 100)
+    elif rc == "loli":
+        fill_rc_stats(usrnm, 80, 150, 1, 6, 6, 8, 150)
+    elif rc == "mlok":
+        fill_rc_stats(usrnm, 50, 150, 5, 7, 3, 8, 100)
+    elif rc == "skeli":
+        fill_rc_stats(usrnm, 150, 80, 8, 1, 8, 1, 50)
+
+    if clss == "soldier":
+
+
+
 
 create_character('aki', 'loli', 'sniper')
