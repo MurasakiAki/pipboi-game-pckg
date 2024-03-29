da9a261f162d21e2eff3eb15930a924b1bad8a6f
MurasakiAki <123252526+MurasakiAki@users.noreply.github.com>
2024-03-29 17:28:30 +0100

    working on commands

--- wastelanders/src/game.sh
+++ wastelanders/src/game.sh
@@ -2,23 +2,58 @@
 
 . system.h
 . character.h
+. item.h
 
 source menu.sh
 
+BRED='\033[1;31m'
+NONE='\033[0m'
 LEVEL=1
 
-# init player
+# item weapon is special item
+item weapon
+weapon.init "Hacksaw" 6 15 #description = stm per use, quantity = damage
 
-echo_menu
+item syringe
+syringe.init "Syringe" "Will heal you for 25% of your max health, uses 5 stamina." 5
+
+item smoke_bomb
+smoke_bomb.init "Smoke Bomb" "Decreases stamina of your enemy for 3 turns, uses 6 stamina." 2
+
+item molotov
+molotov.init "Molotov" "Hurts enemy for 10% of your damage for 3 turns at the begining of each turn, uses 4 stamina." 6
 
 character player
-player.init "aki" 100 20
+player.init "aki" 100 10 $(weapon.quantity)
 
 character enemy
-enemy.init "zombie" 20 5
+enemy.init "zombie" 20 10 5
 
+meassage="What will you do?"
+echo_menu
 until [ "$(player.current_health)" -lt "0" ] || [ "$(player.current_health)" == "0" ]; do
-
-    player.current_health = $(($(player.current_health)-12))
-    player.current_health
+    echo -e $meassage
+    read -p "Your action: " action
+    if [ "$action" == "1" ]; then
+        echo_menu
+        echo 1
+    elif [ "$action" == "2" ]; then
+        echo_menu
+        echo 2
+    elif [ "$action" == "3" ]; then
+        echo_menu
+        echo 3
+    elif [ "$action" == "4" ]; then
+        echo_menu
+        echo 4
+    elif [ "$action" == "5" ]; then
+        echo_menu
+        echo 5
+    elif [ "$action" == "6" ]; then
+        echo_info_menu
+        echo 6
+    else
+        echo_menu
+        meassage=${BRED}"Incorrect action"${NONE}
+    fi
 done
--- wastelanders/src/menu.sh
+++ wastelanders/src/menu.sh
@@ -1,24 +1,66 @@
 #!/bin/bash
 
 RED='\033[0;31m'
+BRED='\033[1;31m'
+ORANGE='\033[1;33m'
 GREEN='\033[0;32m'
+BGREEN='\033[1;32m'
 YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
+BBLUE='\033[1;34m'
+GRAY='\033[1;30m'
+CYAN='\033[0;36m'
 NONE='\033[0m'
 
 function echo_menu() {
-    echo -e "=============================================="
+    player_name=$(player.name)
+    player_chealth=$(player.current_health)
+    player_cstamina=$(player.current_stamina)
+    player_name_len=${#player_name}
+    player_name_line="| $player_name                                 "
+    player_name_line="${player_name_line:0:$((${#player_name_line} - $player_name_len))}"
+    
+    health_space="                       "
+    if [ ${#player_chealth} -eq 2 ]; then
+        health_space="$health_space "
+    elif [ ${#player_chealth} -eq 1 ]; then
+        health_space="$health_space  "
+    fi
+
+    stamina_space="                        "
+    if [ ${#player_cstamina} -eq 1 ]; then
+        health_space="$health_space "
+    fi
+
+    clear
+    echo -e "/============================================\\"
     echo -e "|                                            |"
     echo -e "|                                            |"
     echo -e "|                                            |"
     echo -e "|                                            |"
     echo -e "|                                            |"
     echo -e "|--------------------------------------------|"
-    echo -e "|                                            |"
-    echo -e "|                                            |"
-    echo -e "|                                            |"
-    echo -e "| 1) ${RED}ATTACK${NONE}   3) ${BLUE}DEFEND${NONE}                      |"
-    echo -e "| 2) ${GREEN}HEAL${NONE}     4) ${YELLOW}RUN${NONE}                         |"
-    echo -e "=============================================="
+    echo -e "$player_name_line""|  ${BBLUE}ITEMS${NONE}  |"
+    echo -e "| HP:${BRED}$(player.current_health)/$(player.max_health)${NONE}$health_space| ${BRED}SYR${NONE} "$(syringe.quantity)"/9 |"
+    echo -e "| STM:${BGREEN}$(player.current_stamina)/$(player.max_stamina)${NONE}                        | ${GRAY}SMB${NONE} "$(smoke_bomb.quantity)"/9 |"
+    echo -e "| 1) ${RED}ATTACK${NONE}   3) ${BLUE}DEFEND${NONE}   5) ${GRAY}USE${NONE}   | ${ORANGE}MLT${NONE} "$(molotov.quantity)"/9 |"
+    echo -e "| 2) ${BRED}HEAL${NONE}     4) ${YELLOW}RUN${NONE}      6) ${CYAN}INFO${NONE}  |         |"
+    echo -e "\============================================/"
 
+}
+
+function echo_info_menu() {
+    echo -e "/============================================\\"
+    echo -e "| ${CYAN}INFO MENU${NONE}                                  |"
+    echo -e "| 1) ${RED}ENEMY${NONE}                                |"
+    echo -e "| 2) SYR                                     |"
+    echo -e "| 3) SMB                                           |"
+    echo -e "| 4) MLT                                           |"
+    echo -e "| 5) DEFEND                                           |"
+    echo -e "| 6) RUN                                           |"
+    echo -e "|                                            |"
+    echo -e "|                                            |"
+    echo -e "|                                            |"
+    echo -e "|                                            |"
+    echo -e "\============================================/"
 }
