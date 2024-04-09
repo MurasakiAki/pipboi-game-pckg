cb3760667982683d2e2468f39a098967084ba141
MurasakiAki <akkishia88@gmail.com>
2024-04-09 10:04:55 +0200

    shop menu

---
+++ wastelanders/src/shop_menus.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function echo_shop_menu() {
+    clear
+    echo -e "/============================================\\"
+    echo -e "|                    SHOP                    |"
+    echo -e "|                                            |"
+    echo -e "|                                            |"
+    echo -e "|                                            |"
+    echo -e "|                                            |"
+    echo -e "|                                            |"
+    echo -e "|                                            |"
+    echo -e "|                                            |"
+    echo -e "|                                            |"
+    echo -e "|                                            |"
+    echo -e "|                                            |"
+    echo -e "\============================================/"
+}
