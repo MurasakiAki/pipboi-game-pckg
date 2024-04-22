b44f9dc952fe2b38e14f4cf8706f1fc8fa334368
MurasakiAki <akkishia88@gmail.com>
2024-04-22 11:09:03 +0200

    better setup

--- game-pckg-setup.sh
+++ game-pckg-setup.sh
@@ -3,9 +3,14 @@
 pipboi_path=$(find / -type d -name "pipboi" 2>/dev/null)
 
 if [ -n "$pipboi_path" ]; then
-    echo pipboi exists
-    mv "$PWD" "$pipboi_path/mods"
-    echo Succesfully integrated pipboi-game-pckg into PIPBOI system
+    echo "PIPBOI exists"
+    for game in */ ; do
+        if [ -d "$game" ]; then
+            mv "$game" "$pipboi_path/mods"
+            echo "Moved $game to $pipboi_path/mods"
+        fi
+    done
+    echo "Successfully integrated pipboi-game-pckg into PIPBOI system"
 else
-    echo PIPBOI not installed
+    echo "PIPBOI not installed"
 fi
